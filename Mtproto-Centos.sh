c3d5e958b3210e2bc48815f9f7fd59fcc840eb4d
vahidmajidi <45297578+vahidmajidi@users.noreply.github.com>
2018-12-02 15:09:51 +0330

    Update and rename MTProto-NodeJs-FastSetup-CentOS.sh to Mtproto-Centos.sh

--- Mtproto-Centos.sh
+++ Mtproto-Centos.sh
@@ -1,7 +1,3 @@
-##################################################
-# Anything wrong? Find me via telegram:@MTP_2018 #
-##################################################
-
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
