55081cad9246c9a0a90e58487a2f4b7ea01f4a48
vahidmajidi <45297578+vahidmajidi@users.noreply.github.com>
2018-12-02 13:38:33 +0330

    Update Mtproto-Centos.sh

--- Mtproto-Centos.sh
+++ Mtproto-Centos.sh
@@ -1,3 +1,7 @@
+##################################################
+# VMS #
+##################################################
+
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
